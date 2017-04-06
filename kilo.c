93c6e884cf0a11875522f3c4b644e402254a69ae
Tim Sneath <tims@microsoft.com>
2017-04-05 22:13:08 -0700

    finish screen sizing algorithm

--- kilo.c
+++ kilo.c
@@ -29,6 +29,10 @@ void die(const char *s)
   write(STDOUT_FILENO, "\x1b[H", 3);
 
   perror(s);
+
+  /* because perror will only print \n and we're still in raw mode */
+  printf("\r");
+
   exit(1);
 }
 
@@ -69,13 +73,43 @@ char editorReadKey()
   return c;
 }
 
+int getCursorPosition(int *rows, int *cols)
+{
+  char buf[32];
+  unsigned int i = 0;
+
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+    return -1;
+
+  while (i < sizeof(buf) - 1)
+  {
+    if (read(STDIN_FILENO, &buf[i], 1) != 1)
+      break;
+    if (buf[i] == 'R')
+      break;
+    i++;
+  }
+  buf[i] = '\0';
+
+  if (buf[0] != '\x1b' || buf[1] != '[')
+    return -1;
+  if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+    return -1;
+
+  return 0;
+}
+
 int getWindowSize(int *rows, int *cols)
 {
   struct winsize ws;
 
   if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
   {
-    return -1;
+    if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+    {
+      return -1;
+    }
+    return getCursorPosition(rows, cols);
   }
   else
   {
@@ -91,7 +125,12 @@ void editorDrawRows()
   int y;
   for (y = 0; y < E.screenrows; y++)
   {
-    write(STDOUT_FILENO, "~\r\n", 3);
+    write(STDOUT_FILENO, "~", 1);
+
+    if (y < E.screenrows - 1)
+    {
+      write(STDOUT_FILENO, "\r\n", 2);
+    }
   }
 }
 
