4cb5677fc5a61f8cd93717883e505890f36ad7b3
Tim Sneath <tims@microsoft.com>
2017-04-05 21:47:10 -0700

    basic key input working

--- kilo.c
+++ kilo.c
@@ -15,7 +15,8 @@
 struct termios orig_termios;
 
 /*** terminal ***/
-void die(const char *s) {
+void die(const char *s)
+{
   write(STDOUT_FILENO, "\x1b[2J", 4);
   write(STDOUT_FILENO, "\x1b[H", 3);
 
@@ -23,12 +24,14 @@ void die(const char *s) {
   exit(1);
 }
 
-void disableRawMode() {
+void disableRawMode()
+{
   if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
     die("tcsetattr");
 }
 
-void enableRawMode() {
+void enableRawMode()
+{
   if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
     die("tcgetattr");
   atexit(disableRawMode);
@@ -45,11 +48,13 @@ void enableRawMode() {
     die("tcsetattr");
 }
 
-char editorReadKey() {
+char editorReadKey()
+{
   int nread;
   char c;
 
-  while ((nread = read(STDIN_FILENO, &c, 1)) != -1) {
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+  {
     if (nread == -1 && errno != EAGAIN)
       die("read");
   }
@@ -57,23 +62,32 @@ char editorReadKey() {
 }
 
 /*** output ***/
-void editorDrawRows() {
+void editorDrawRows()
+{
   int y;
-  for (y = 0; y < 24; y++) {
+  for (y = 0; y < 24; y++)
+  {
     write(STDOUT_FILENO, "~\r\n", 3);
   }
 }
 
-void editorRefreshScreen() {
+void editorRefreshScreen()
+{
   write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  editorDrawRows();
+
   write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 /*** input ***/
-void editorProcessKeypress() {
+void editorProcessKeypress()
+{
   char c = editorReadKey();
 
-  switch (c) {
+  switch (c)
+  {
   case CTRL_KEY('q'):
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
@@ -83,10 +97,11 @@ void editorProcessKeypress() {
 }
 
 /*** init ***/
-int main() {
+int main()
+{
   enableRawMode();
-
-  while (1) {
+  while (1)
+  {
     editorRefreshScreen();
     editorProcessKeypress();
   }
