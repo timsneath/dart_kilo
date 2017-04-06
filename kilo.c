8fcf35da6ca072aef0792964aadd94fa26771f84
Tim Sneath <tims@microsoft.com>
2017-04-05 20:18:53 -0700

    clang-format

--- kilo.c
+++ kilo.c
@@ -4,27 +4,28 @@
 
 struct termios orig_termios;
 
-void enableRawMode()
-{
-    tcgetattr(STDIN_FILENO, &orig_termios);
-    atexit(disableRawMode);
+void enableRawMode() {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON);
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO | ICANON);
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
-void disableRawMode()
-{
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
-}
+void disableRawMode() { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); }
 
-int main()
-{
-    enableRawMode();
+int main() {
+  enableRawMode();
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
-    return 0;
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (isprint(c)) {
+      printf("%d ('%c')\n", c, c);
+    } else {
+      printf("%d\n", c);
+    }
+  }
+  return 0;
 }
