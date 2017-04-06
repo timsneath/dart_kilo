429ae578c04f6d3ddd68e64e00d73517f5d31e1f
Tim Sneath <tims@microsoft.com>
2017-04-06 16:22:32 -0700

    Add PAGE_UP and PAGE_DOWN

--- kilo.c
+++ kilo.c
@@ -14,7 +14,16 @@
 #define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k)&0x1f)
 
-enum editorKey { ARROW_UP = 1000, ARROW_DOWN, ARROW_LEFT, ARROW_RIGHT, PAGE_UP, PAGE_DOWN };
+enum editorKey {
+  ARROW_UP = 1000,
+  ARROW_DOWN,
+  ARROW_LEFT,
+  ARROW_RIGHT,
+  HOME_KEY,
+  END_KEY,
+  PAGE_UP,
+  PAGE_DOWN
+};
 
 /*** data ***/
 struct editorConfig {
@@ -79,15 +88,28 @@ int editorReadKey() {
       return '\x1b';
 
     if (seq[0] == '[') {
-      switch (seq[1]) {
-      case 'A':
-        return ARROW_UP;
-      case 'B':
-        return ARROW_DOWN;
-      case 'C':
-        return ARROW_RIGHT;
-      case 'D':
-        return ARROW_LEFT;
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1)
+          return '\x1b';
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+          case '5':
+            return PAGE_UP;
+          case '6':
+            return PAGE_DOWN;
+          }
+        }
+      } else {
+        switch (seq[1]) {
+        case 'A':
+          return ARROW_UP;
+        case 'B':
+          return ARROW_DOWN;
+        case 'C':
+          return ARROW_RIGHT;
+        case 'D':
+          return ARROW_LEFT;
+        }
       }
     }
     return '\x1b';
@@ -240,6 +262,21 @@ void editorProcessKeypress() {
     write(STDOUT_FILENO, "\x1b[H", 3);  /* position cursor at origin */
     exit(0);
     break;
+
+  case HOME_KEY:
+    E.cx = 0;
+    break;
+
+  case END_KEY:
+    E.cx = E.screencols - 1;
+    break;
+
+  case PAGE_UP:
+  case PAGE_DOWN: {
+    int times = E.screenrows;
+    while (times--)
+      editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+  } break;
 
   case ARROW_UP:
   case ARROW_DOWN:
