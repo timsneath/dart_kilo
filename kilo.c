1ea16a1ce98cba4e3b0a36b2844f3545a85e28b6
Tim Sneath <tims@microsoft.com>
2017-04-05 21:53:52 -0700

    add detection for terminal size

--- kilo.c
+++ kilo.c
@@ -2,17 +2,25 @@
 
 /*** includes ***/
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
-#include <errno.h>
 
 /*** defines ***/
 #define CTRL_KEY(k) ((k)&0x1f)
 
 /*** data ***/
-struct termios orig_termios;
+struct editorConfig
+{
+  int screenrows;
+  int screencols;
+  struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 /*** terminal ***/
 void die(const char *s)
@@ -26,17 +34,17 @@ void die(const char *s)
 
 void disableRawMode()
 {
-  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
     die("tcsetattr");
 }
 
 void enableRawMode()
 {
-  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
     die("tcgetattr");
   atexit(disableRawMode);
 
-  struct termios raw = orig_termios;
+  struct termios raw = E.orig_termios;
   raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
   raw.c_oflag &= ~(OPOST);
   raw.c_cflag |= (CS8);
@@ -61,11 +69,27 @@ char editorReadKey()
   return c;
 }
 
+int getWindowSize(int *rows, int *cols)
+{
+  struct winsize ws;
+
+  if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+  {
+    return -1;
+  }
+  else
+  {
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+    return 0;
+  }
+}
+
 /*** output ***/
 void editorDrawRows()
 {
   int y;
-  for (y = 0; y < 24; y++)
+  for (y = 0; y < E.screenrows; y++)
   {
     write(STDOUT_FILENO, "~\r\n", 3);
   }
@@ -97,9 +121,19 @@ void editorProcessKeypress()
 }
 
 /*** init ***/
+void initEditor()
+{
+  if (getWindowSize(&E.screenrows, &E.screencols) == -1)
+  {
+    die("getWindowSize");
+  }
+}
+
 int main()
 {
   enableRawMode();
+  initEditor();
+
   while (1)
   {
     editorRefreshScreen();
