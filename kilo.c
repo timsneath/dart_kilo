b5ef6a81dd8754d72381f6cda18351e31e96360c
Tim Sneath <tims@microsoft.com>
2017-04-06 15:19:09 -0700

    add welcome message

--- kilo.c
+++ kilo.c
@@ -11,6 +11,7 @@
 #include <unistd.h>
 
 /*** defines ***/
+#define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k)&0x1f)
 
 /*** data ***/
@@ -25,7 +26,7 @@ struct editorConfig E;
 /*** terminal ***/
 void die(const char *s) {
   write(STDOUT_FILENO, "\x1b[2J", 4); /* clear screen */
-  write(STDOUT_FILENO, "\x1b[H", 3);  /* position cursor at origin */ 
+  write(STDOUT_FILENO, "\x1b[H", 3);  /* position cursor at origin */
 
   perror(s);
 
@@ -72,7 +73,7 @@ int getCursorPosition(int *rows, int *cols) {
   char buf[32];
   unsigned int i = 0;
 
-  /* retrieve current cursor position to STDIN */ 
+  /* retrieve current cursor position to STDIN */
   if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
     return -1;
 
@@ -131,17 +132,30 @@ void abAppend(struct abuf *ab, const char *s, int len) {
   ab->len += len;
 }
 
-void abFree(struct abuf *ab) 
-{
-  free(ab->b);
-}
+void abFree(struct abuf *ab) { free(ab->b); }
 
 /*** output ***/
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    abAppend(ab, "~", 1);
+    if (y == E.screenrows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome),
+                               "Kilo editor -- version %s", KILO_VERSION);
+      if (welcomelen > E.screencols)
+        welcomelen = E.screencols;
+      int padding = (E.screencols - welcomelen) / 2;
+      if (padding) {
+        abAppend(ab, "~", 1);
+        padding--;
+      }
+      while (padding--) abAppend(ab, " ", 1);
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
+    }
 
+    abAppend(ab, "\x1b[K", 3); /* erase in line */
     if (y < E.screenrows - 1) {
       abAppend(ab, "\r\n", 2);
     }
@@ -152,13 +166,12 @@ void editorRefreshScreen() {
   struct abuf ab = ABUF_INIT;
 
   abAppend(&ab, "\x1b[?25l", 6); /* hide cursor */
-  abAppend(&ab, "\x1b[2J", 4);   /* clear screen */
   abAppend(&ab, "\x1b[H", 3);    /* position cursor at origin */
 
   editorDrawRows(&ab);
 
-  abAppend(&ab,"\x1b[H", 3);     /* position cursor at origin */
-  abAppend(&ab, "\x1b[?25h", 6); /* show cursor */ 
+  abAppend(&ab, "\x1b[H", 3);    /* position cursor at origin */
+  abAppend(&ab, "\x1b[?25h", 6); /* show cursor */
 
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
@@ -171,7 +184,7 @@ void editorProcessKeypress() {
   switch (c) {
   case CTRL_KEY('q'):
     write(STDOUT_FILENO, "\x1b[2J", 4); /* clear screen */
-    write(STDOUT_FILENO, "\x1b[H", 3);  /* position cursor at origin */ 
+    write(STDOUT_FILENO, "\x1b[H", 3);  /* position cursor at origin */
     exit(0);
     break;
   }
