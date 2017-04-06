16d16a0ce913bbd497b26e5b3962851717f83bef
Tim Sneath <tims@microsoft.com>
2017-04-05 19:51:55 -0700

    add disableRawMode on app exit

--- kilo.c
+++ kilo.c
@@ -1,15 +1,23 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
 void enableRawMode()
 {
-    struct termios raw;
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
 
-    tcgetattr(STDIN_FILENO, &raw);
-
-    raw.c_lflag &= ~(ECHO);
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+void disableRawMode()
+{
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
 int main()
