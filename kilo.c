f334624c2426460d2f446ff67ba4ce1923141a9c
Tim Sneath <tims@microsoft.com>
2017-06-19 14:44:51 -0700

    handle PAGE_DOWN/PAGE_UP/END_KEY

--- kilo.c
+++ kilo.c
@@ -43,6 +43,7 @@ typedef struct erow {
 
 struct editorConfig {
   int cx, cy;
+  int rx;
   int rowoff;
   int coloff;
   int screenrows;
@@ -190,7 +191,19 @@ int getWindowSize(int *rows, int *cols) {
   }
 }
 
-/*** file i/o ***/
+/*** row operations ***/
+int editorRowCxToRx(erow *row, int cx) {
+  int rx = 0;
+  int j;
+  for (j = 0; j < cx; j++) {
+    if (row->chars[j] == '\t') {
+      rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
+    }
+    rx++;
+  }
+  return rx;
+}
+
 void editorUpdateRow(erow *row) {
   int tabs = 0;
   int j;
@@ -230,6 +243,7 @@ void editorAppendRow(char *s, size_t len) {
   E.numrows++;
 }
 
+/*** file i/o ***/
 void editorOpen(char *filename) {
   FILE *fp = fopen(filename, "r");
   if (!fp) die("fopen");
@@ -270,10 +284,15 @@ void abFree(struct abuf *ab) { free(ab->b); }
 
 /*** output ***/
 void editorScroll() {
+  E.rx = 0;
+  if (E.cy < E.numrows) {
+    E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+  }
+
   if (E.cy < E.rowoff) E.rowoff = E.cy;
   if (E.cy >= E.rowoff + E.screenrows) E.rowoff = E.cy - E.screenrows + 1;
-  if (E.cx < E.coloff) E.coloff = E.cx;
-  if (E.cx >= E.coloff + E.screencols) E.coloff = E.cx - E.screencols + 1;
+  if (E.rx < E.coloff) E.coloff = E.rx;
+  if (E.rx >= E.coloff + E.screencols) E.coloff = E.rx - E.screencols + 1;
 }
 
 void editorDrawRows(struct abuf *ab) {
@@ -323,7 +342,7 @@ void editorRefreshScreen() {
   /* position cursor at current coords */
   char buf[32];
   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
-           (E.cx - E.coloff) + 1);
+           (E.rx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6); /* show cursor */
@@ -403,6 +422,7 @@ void editorProcessKeypress() {
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.rx = 0;
   E.rowoff = 0;
   E.coloff = 0;
   E.numrows = 0;
