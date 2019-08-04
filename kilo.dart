5513169d2c7e0529fa83d2b88266afaf288b4e54
Tim Sneath <tim@sneath.org>
2019-08-04 16:34:12 -0700

    Use refactored console class

--- kilo.dart
+++ kilo.dart
@@ -1,24 +1,52 @@
 import 'dart:io';
-import 'termlib/termlib.dart';
 
-final terminal = TermLib();
+import 'termlib/console.dart';
+
+final console = Console();
 
 const controlQ = 0x11;
 
+void die(String message) {
+  console.clearScreen();
+  console.resetCursor();
+  stdout.write(message);
+  exit(1);
+}
+
+// output
+void editorDrawRows() {
+  for (int y = 0; y < console.windowHeight; y++) {
+    stdout.write('~\r\n');
+  }
+}
+
+void editorRefreshScreen() {
+  console.clearScreen();
+  console.resetCursor();
+
+  editorDrawRows();
+
+  console.resetCursor();
+}
+
+// input
 void editorProcessKeypress() {
   final codeUnit = stdin.readByteSync();
 
   switch (codeUnit) {
     case controlQ:
+      console.clearScreen();
+      console.resetCursor();
       exit(0);
       break;
   }
 }
 
 main(List<String> arguments) {
-  terminal.enableRawMode();
+  console.enableRawMode();
 
   while (true) {
+    editorRefreshScreen();
     editorProcessKeypress();
   }
 }
