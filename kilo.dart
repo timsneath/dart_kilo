c0c61d055f0c170a75a003d24569709fd1254232
Tim Sneath <tim@sneath.org>
2019-08-04 16:04:22 -0700

    Refactor input code

--- kilo.dart
+++ kilo.dart
@@ -3,33 +3,22 @@ import 'termlib/termlib.dart';
 
 final terminal = TermLib();
 
-int controlCharacter(String key) => key.codeUnitAt(0) & 0x1F;
+const controlQ = 0x11;
 
-bool isCodeUnitNonPrintable(int rawCharCode) {
-  if (rawCharCode >= 0x00 && rawCharCode <= 0x1f) return true;
-  if (rawCharCode == 0x7f) return true;
-  return false;
+void editorProcessKeypress() {
+  final codeUnit = stdin.readByteSync();
+
+  switch (codeUnit) {
+    case controlQ:
+      exit(0);
+      break;
+  }
 }
 
 main(List<String> arguments) {
   terminal.enableRawMode();
 
-  int codeUnit;
-  String char;
-
   while (true) {
-    codeUnit = stdin.readByteSync();
-    if (codeUnit == -1) break;
-
-    char = String.fromCharCode(codeUnit);
-
-    if (isCodeUnitNonPrintable(codeUnit)) {
-      stdout.write("$codeUnit\r\n");
-    } else {
-      stdout.write("$codeUnit ('$char')\r\n");
-    }
-    if (codeUnit == controlCharacter('Q')) break;
+    editorProcessKeypress();
   }
-
-  return 0;
 }
