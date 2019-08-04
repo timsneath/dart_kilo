a06d04ef4b54c6c0d934fc0e68944300c28c6c07
Tim Sneath <tim@sneath.org>
2019-08-04 15:45:32 -0700

    Full raw mode sequence

--- termlib/termlib.c
+++ termlib/termlib.c
@@ -1,7 +1,6 @@
 // Compile with (Linux):
 //    gcc -shared -fPIC -o termlib/termlib.so termlib/termlib.c
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
@@ -10,20 +9,22 @@ struct termios orig_termios;
 
 void disableRawMode()
 {
-    puts("Disabling raw mode");
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
 void enableRawMode()
 {
-    puts("Enabling raw mode");
     tcgetattr(STDIN_FILENO, &orig_termios);
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
-    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
